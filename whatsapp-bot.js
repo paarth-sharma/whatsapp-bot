95f1c3bc3c59e744bf0c3e12c6d56f994e77146c
paarth-sharma <paarthsharma2003@gmail.com>
2022-03-08 23:08:20 +0530

    resolved the selector values for contact name

--- whatsapp-bot.js
+++ whatsapp-bot.js
@@ -14,16 +14,16 @@ const puppeteer = require("puppeteer");
 
         //Navigates to Whatsapp
         await page.goto("https://web.whatsapp.com/");
-        await page.waitForSelector("._1MXsz");
+        await page.waitForSelector("._3hKpJ");
         await delay(5000);
 
         //Change to contact you want to send messages to
         const contactName = "ACM WhatsApp bot";
         await page.click(`span[title='${contactName}']`);
-        await page.waitForSelector("._3uMse");
+        await page.waitForSelector("._1LbR4");
 
         //Finds the message bar and focuses on it
-        const editor = await page.$("div[data-tab='1']"); //whatsapp web has div "data-tab=1" for message inputs
+        const editor = await page.$("div[data-tab='10']"); //whatsapp web has div "data-tab=1" for message inputs
         await editor.focus();
 
         //Amount of messages you want to send
